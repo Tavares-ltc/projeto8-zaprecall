da1007b73d9584ec4df1b2e6a372ad408ece21d1
Lucas Tavares <l.tavaresc@hotmail.com>
2022-07-25 17:53:45 -0300

    Foi identificado um erro data undefined algumas vezes ao carregar a pagina, o sorteio das perguntas foi motivo para outro componente no intuito de resolver.

--- src/launch.js
+++ src/launch.js
@@ -2,6 +2,34 @@ import React from "react";
 import Recall from "./recall";
 import Logo from "./logo";
 
+const questionsList = [
+    { question: 'O que é JSX?', answer: 'Uma extensão de linguagem do JavaScript' },
+    { question: 'O React é __ ', answer: 'uma biblioteca JavaScript para construção de interfaces' },
+    { question: 'Componentes devem iniciar com __ ', answer: 'letra maiúscula' },
+    { question: 'Podemos colocar __ dentro do JSX  ', answer: 'expressões' },
+    { question: 'O ReactDOM nos ajuda __ ', answer: 'interagindo com a DOM para colocar componentes React na mesma' },
+    { question: 'Usamos o npm para', answer: 'gerenciar os pacotes necessários e suas dependências' },
+    { question: 'Usamos props para __', answer: 'passar diferentes informações para componentes' },
+    { question: 'Usamos estado (state) para __', answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente' },
+]
+const sortedNumbers = []
+const questions = []
+
+function sort() {
+    for (let i = 0; i < 4; i++) {
+        
+        let num = Math.floor(Math.random() * 9)
+        if (sortedNumbers.includes(num)) {
+            i--
+        }
+        else {
+            sortedNumbers.push(num)
+            questions.push(questionsList[num])
+        }
+    }
+}
+sort();
+
 function LaunchPage() {
     const [tela, setTela] = React.useState(true)
 
@@ -14,7 +42,7 @@ function LaunchPage() {
                         <h2>Iniciar Recall!</h2>
                     </div>
                 </div>
-            ) : (<Recall setTela={setTela} />)}
+            ) : (<Recall setTela={setTela} questions={questions}/>)}
         </>
     )
 }
--- src/recall.js
+++ src/recall.js
@@ -1,49 +1,24 @@
 import React from 'react'
 import Footer from './footer'
 import AskCards from './cardstate'
+console.log("Foi identificado um erro ao carrergar a página, se acontecer tente recarregar.")
 
-const questionsList = [
-    { question: 'O que é JSX?', answer: 'Uma extensão de linguagem do JavaScript' },
-    { question: 'O React é __ ', answer: 'uma biblioteca JavaScript para construção de interfaces' },
-    { question: 'Componentes devem iniciar com __ ', answer: 'letra maiúscula' },
-    { question: 'Podemos colocar __ dentro do JSX  ', answer: 'expressões' },
-    { question: 'O ReactDOM nos ajuda __ ', answer: 'interagindo com a DOM para colocar componentes React na mesma' },
-    { question: 'Usamos o npm para', answer: 'gerenciar os pacotes necessários e suas dependências' },
-    { question: 'Usamos props para __', answer: 'passar diferentes informações para componentes' },
-    { question: 'Usamos estado (state) para __', answer: 'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente' },
-]
-
-const sortedNumbers = []
-const questions = []
-function sort() {
-    for (let i = 0; i < 4; i++) {
-
-        let num = Math.floor(Math.random() * 9)
-        if (sortedNumbers.includes(num)) {
-            i--
-        }
-        else {
-            sortedNumbers.push(num)
-            questions.push(questionsList[num])
-        }
-    }
-}
-sort();
-
-function Recall({setTela}) {
+function Recall({ setTela, questions }) {
     const [footerResult, setFooterResult] = React.useState(0)
     const [resultList, setResultList] = React.useState([])
 
-    return (
-        <div className="recallPage">
-            <div className="logo">
-                <img src="img/logo-pequeno.png"></img>
-                <h1>ZapRecall</h1>
+
+
+        return (
+            <div className="recallPage">
+                <div className="logo">
+                    <img src="img/logo-pequeno.png"></img>
+                    <h1>ZapRecall</h1>
+                </div>
+                {questions.map((data, index) => (<AskCards question={data.question} answer={data.answer} index={index} setResultList={setResultList} resultList={resultList} setFooterResult={setFooterResult} footerResult={footerResult} />))}
+                <Footer resultList={resultList} footerResult={footerResult} setTela={setTela}></Footer>
             </div>
-            {questions.map((data, index) => (<AskCards question={data.question} answer={data.answer} index={index} setResultList={setResultList} resultList={resultList} setFooterResult={setFooterResult} footerResult={footerResult} />))}
-            <Footer resultList={resultList} footerResult={footerResult} setTela={setTela}></Footer>
-        </div>
-    )
+        )
 }
 
 
